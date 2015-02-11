afab701d736808c95ef3f3757d43ea58b56e3d6f
udaraseneviratnesmuc <udarachathurangas@gmail.com>
2015-02-12 00:11:08 +0530

    put a background, bug fixes in level changing, implementation for attemt counting, reset function and number pad implementation

---
+++ js/JSEventHandlers.js
@@ -0,0 +1,63 @@
+function JSEventHandlers(){
+    this.interval;
+    this.intervalSwitch = 0;
+}
+
+var x = document.getElementById("answer");
+if (x.addEventListener) {
+    x.addEventListener("keydown", function(e){
+    	if(e.keyCode === 13 ){
+    		mathInterface.compareResults(document.getElementById('answer').value);
+    		document.getElementById('answer').value = "" ;
+            resultsGenerator.triggured = true;
+           // mathInterface.callOperation(3, "summation", 2);
+    	}
+    });	
+} else if (x.attachEvent) {
+    x.attachEvent("keydown", function(e){
+    	if(e.keyCode === 13 ){
+    		mathInterface.compareResults(document.getElementById('answer').value);
+    		document.getElementById('answer').value = "" ;
+            resultsGenerator.triggured = true;
+           // mathInterface.callOperation(3, "summation", 2);
+    	}
+ });
+}
+
+var startBtn = document.getElementById("start_btn");
+if (startBtn.addEventListener) {
+    startBtn.addEventListener("click", function(e){
+        if(jsEventHandlers.intervalSwitch == 0){
+            jsEventHandlers.interval = setInterval(function(){ mathInterface.start();}, 5000);
+            startBtn.value = "Pause";
+            jsEventHandlers.intervalSwitch = 1;
+            document.getElementById('answer').disabled = false;
+            document.getElementById('mathematicalStatement').innerHTML = "";
+            document.getElementById("answer").focus();
+        } else if(jsEventHandlers.intervalSwitch == 1){
+             clearInterval(jsEventHandlers.interval);
+             startBtn.value = "Start";
+             jsEventHandlers.intervalSwitch = 0;
+             resultsGenerator.systemResult = null;
+             document.getElementById('answer').disabled = true;
+             document.getElementById('mathematicalStatement').innerHTML = "Paused";
+        }
+    }); 
+}
+
+var resetBtn = document.getElementById("reset_btn");
+if (resetBtn.addEventListener) {
+    resetBtn.addEventListener("click", function(e){
+        clearInterval(jsEventHandlers.interval);
+        mathInterface.reset();
+        document.getElementById('answer').value = "";
+        document.getElementById("answer").focus();
+        jsEventHandlers.interval = setInterval(function(){ mathInterface.start();}, 5000);
+    }); 
+}
+
+
+
+
+var jsEventHandlers = new JSEventHandlers();
+    
---
+++ js/MindGamesConfigurations.js
@@ -0,0 +1,7 @@
+function MindGamesConfigurations(){
+	this.markLevels = [200, 600, 1800];
+	this.markLevelsNames = {0:"Primary Level", 0.1:"Ordinary Level", 0.2:"Ordinary Level", 0.3:"Ordinary Level", 1:"Ordinary Level", 2:"Advanced Level", 3:"Super Advanced Level"};
+	this.mathOperations = ["summation", "deduction", "multiplication", "division"];
+}
+
+var mindGamesConfigurations = new MindGamesConfigurations();
