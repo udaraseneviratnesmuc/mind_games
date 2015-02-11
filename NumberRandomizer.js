c572bc5cb5541ac479f9c6d642c0b89f5138cf0f
udaraseneviratnesmuc <udarachathurangas@gmail.com>
2015-02-12 00:24:42 +0530

    Deleted Unwanted files

--- NumberRandomizer.js
+++
@@ -1,9 +0,0 @@
-function NumberRandomizer(){
-}
-
-NumberRandomizer.prototype.randomNumberGen = function(digitRange) {
-	var decPlaces = parseInt("1" + (Array(digitRange).join("0")));
-	return (Math.floor(Math.random() * decPlaces) + 1);
-}
-
-var numberRandomizer = new NumberRandomizer();
