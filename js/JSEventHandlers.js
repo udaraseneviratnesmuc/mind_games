f44e264437bf4d92b1b017fc5e5baf585d2d67fa
udaraseneviratnesmuc <udarachathurangas@gmail.com>
2015-02-14 22:49:50 +0530

    implemented numpad enter func and disable hardware keyboard

--- js/JSEventHandlers.js
+++ js/JSEventHandlers.js
@@ -24,6 +24,13 @@ if (x.addEventListener) {
  });
 }
 
+document.getElementById("answer").onkeydown = function (e) {
+            // if(e.which == 9){
+            //         return false;
+            // }
+    return false;
+}
+
 var startBtn = document.getElementById("start_btn");
 if (startBtn.addEventListener) {
     startBtn.addEventListener("click", function(e){
@@ -60,60 +67,85 @@ var btn1 = document.getElementById("btn1");
 if (btn1.addEventListener) {
     btn1.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(1, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn2 = document.getElementById("btn2");
 if (btn2.addEventListener) {
     btn2.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(2, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn3 = document.getElementById("btn3");
 if (btn3.addEventListener) {
     btn3.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(3, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn4 = document.getElementById("btn4");
 if (btn4.addEventListener) {
     btn4.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(4, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn5 = document.getElementById("btn5");
 if (btn5.addEventListener) {
     btn5.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(5, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn6 = document.getElementById("btn6");
 if (btn6.addEventListener) {
     btn6.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(6, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn7 = document.getElementById("btn7");
 if (btn7.addEventListener) {
     btn7.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(7, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn8 = document.getElementById("btn8");
 if (btn8.addEventListener) {
     btn8.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(8, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn9 = document.getElementById("btn9");
 if (btn9.addEventListener) {
     btn9.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(9, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
     }); 
 }
 var btn0 = document.getElementById("btn0");
 if (btn0.addEventListener) {
     btn0.addEventListener("click", function(e){
         document.getElementById('answer').value = numPad.valuePrinter(0, document.getElementById('answer').value, "string");
+        document.getElementById("answer").focus();
+    }); 
+}
+var btnDel = document.getElementById("btnDel");
+if (btnDel.addEventListener) {
+    btnDel.addEventListener("click", function(e){
+        document.getElementById('answer').value = numPad.backspace(document.getElementById('answer').value);
+        document.getElementById("answer").focus();
+    }); 
+}
+var enter = document.getElementById("enter");
+if (enter.addEventListener) {
+    enter.addEventListener("click", function(e){
+        mathInterface.compareResults(document.getElementById('answer').value);
+        document.getElementById('answer').value = "" ;
+        resultsGenerator.triggured = true;
     }); 
 }
 
