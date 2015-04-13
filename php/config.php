70544caa6d787c68eaad0570b7f07041898e110d
seneviratnesmuc <udarachathurangas@gmail.com>
2015-04-13 23:23:46 +0530

    added new config values

--- php/config.php
+++ php/config.php
@@ -14,11 +14,12 @@ class Config{
 	function statusCodes(){
 		return array(
 		    'login_authorized' => '3200',
+		    'sign_up_success' => '4200',
 		    'login_email_not_exist' => '3401',
 		    'login_pwd_error' => '3402',
 		    'login_unauothorized' => '3400',
 		    'user_no_exist' => '3404',
-		    'login_email_already_available' => '3423',
+		    'login_email_already_available' => '3423'
 		);
 	}
 
