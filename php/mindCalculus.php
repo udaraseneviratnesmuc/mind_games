230770f65f4fdd2699ee3b5b1f1776e6711e8338
seneviratnesmuc <udarachathurangas@gmail.com>
2015-04-13 23:24:52 +0530

    sign up function implementation completed

--- php/mindCalculus.php
+++ php/mindCalculus.php
@@ -11,18 +11,21 @@ class MindCalculus{
 
 	function signUp($email, $pwd){
 		$dbConnection = $this -> connectDB();
-		$userExistence = $this -> checkForUserExistance();
+		$config = new Config();
+		$configValues = $config -> statusCodes();
+		$userExistence = $this -> checkForUserExistance($email);
 		if($userExistence == 0){
 			mysqli_query($dbConnection, "insert into userLogin(userId, email, password) values ('', '$email', '$pwd')");
+			return $configValues['sign_up_success'];
 		}else{
-			echo "E-mail address already available.";
+			return $configValues['login_email_already_available'];
 		}
 		mysqli_close($dbConnection);
 	}
 
 	function submitMarks($email, $pwd, $marks, $attempts, $country){
 		$dbConnection = $this -> connectDB();
-		$user = $this -> checkForUserExistance();
+		$user = $this -> checkForUserExistance($email);
 		if($userExistence == 0){
 			echo "User not exists";
 		}else{
@@ -30,18 +33,19 @@ class MindCalculus{
 		}
 	}
 
-	function checkForUserExistance(){
+	function checkForUserExistance($email){
 		$dbConnection = $this -> connectDB();
-		$userExistence = mysqli_query($dbConnection, "select exists (select 1 from userLogin where email = $email)");
+		$userExistence = mysqli_query($dbConnection, "select exists (select 1 from userLogin where email = '$email')");
+		$userExistenceVal = mysqli_fetch_array($userExistence);
 		mysqli_close($dbConnection);
-		return $userExistence
+		return $userExistenceVal[0];
 	}
 
 	function validateUser(){
 		$dbConnection = $this -> connectDB();
-		$validateEmail = mysqli_query($dbConnection, "select exists (select 1 from userLogin where email = $email)");
+		$validateEmail = mysqli_query($dbConnection, "select exists (select 1 from userLogin where email = '$email')");
 		if($validateEmail == 1){
-			$validatePwd = mysqli_query($dbConnection, "select exists (select 1 from userLogin where email = $email and pwd = $pwd)");
+			$validatePwd = mysqli_query($dbConnection, "select exists (select 1 from userLogin where email = '$email' and pwd = '$pwd')");
 			if($validatePwd == 1){
 				return 202;
 			}else{
