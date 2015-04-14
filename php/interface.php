8131fd442b3d79e9f7940b28e7f4c47f9a40b51c
seneviratnesmuc <udarachathurangas@gmail.com>
2015-04-14 15:15:54 +0530

    included a new function call

--- php/interface.php
+++ php/interface.php
@@ -7,11 +7,15 @@
 	$pwd = isset($_POST['pwd']) ? md5($_POST['pwd']) : null;
 	$marks = isset($_POST['marks']) ? $_POST['marks'] : null;
 	$attempts = isset($_POST['attempts']) ? $_POST['attempts'] : null;
+	$country = isset($_POST['country']) ? $_POST['country'] : null;
 
 	if($class == "mindCalculus"){
 		$mindCalculus = new MindCalculus();
 		if($method == "signup"){
 			echo $mindCalculus -> signup($email, $pwd); 	
+		}
+		if($method == "submitMarks"){
+			echo $mindCalculus -> submitMarks($email, $pwd, $marks, $attempts, $country); 	
 		}
 	}
 
